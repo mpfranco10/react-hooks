0430d590681da92b0c9a08065952c40501391301
Maria <mpfrancog@outlook.com>
2024-06-24 20:58:55 -0500

    added typescript to event listener

---
+++ src/hooks/useEventListener.ts
@@ -0,0 +1,27 @@
+import {
+  MutableRefObject,
+  useEffect,
+  experimental_useEffectEvent as useEffectEvent,
+} from "react";
+
+export const useEventListener = <T extends HTMLElement>(
+  target: T | MutableRefObject<T>,
+  eventName: string,
+  handler: () => void,
+  options: never
+) => {
+  const onEvent = useEffectEvent(handler);
+
+  useEffect(() => {
+    // @ts-expect-error It could be either an element or a ref to the element
+    const targetElement = target.current ?? target;
+
+    if (!targetElement?.addEventListener) return;
+
+    targetElement.addEventListener(eventName, onEvent, options);
+
+    return () => {
+      targetElement.removeEventListener(eventName, onEvent, options);
+    };
+  }, [target, eventName, options]);
+};
