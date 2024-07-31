4f2e25ed054834c40fa261062fcb336510db2d2a
Maria <mpfrancog@outlook.com>
2024-07-30 22:28:16 -0500

    added use interval when

---
+++ src/hooks/useIntervalWhen.ts
@@ -0,0 +1,42 @@
+import {
+  useEffect,
+  experimental_useEffectEvent as useEffectEvent,
+  useCallback,
+  useRef,
+} from "react";
+
+interface IntervalWhenOptions {
+  ms: number;
+  when: boolean;
+  startImmediately: boolean;
+}
+
+export default function useIntervalWhen(
+  cb: () => void,
+  { ms, when, startImmediately }: IntervalWhenOptions
+) {
+  const id = useRef(0);
+  const immediatelyCalled = useRef(startImmediately ? false : null);
+
+  const onTick = useEffectEvent(cb);
+
+  const handleClearInterval = useCallback(() => {
+    window.clearInterval(id.current);
+    immediatelyCalled.current = false;
+  }, []);
+
+  useEffect(() => {
+    if (when) {
+      id.current = window.setInterval(onTick, ms);
+
+      if (startImmediately && !immediatelyCalled.current) {
+        onTick();
+        immediatelyCalled.current = true;
+      }
+
+      return handleClearInterval;
+    }
+  }, [ms, when, startImmediately, handleClearInterval]);
+
+  return handleClearInterval;
+}
