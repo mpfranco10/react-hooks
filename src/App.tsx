4279394f89a2f19dc9965a53c231fac2a5b2e8a1
María Franco <maria.franco@takecommandhealth.com>
2024-11-04 16:36:34 +0900

    hotjar again

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import "./App.css";
 import {
   BatteryDemo,
@@ -32,6 +32,7 @@ import {
   WindowScrollDemo,
   WindowSizeDemo,
 } from "./AuxiliarComponents";
+import { hotjar } from "react-hotjar";
 
 const HookContainer = ({
   title,
@@ -52,137 +53,144 @@ const HookContainer = ({
   </div>
 );
 
-const App = () => (
-  <div>
-    <h1>useHooks rebuild</h1>
-    <p>
-      Project to build from scratch hooks from ui.dev{" "}
-      <a href="https://usehooks.com/" target="_blank">
-        useHooks
-      </a>
-    </p>
-    <div className="hooks-grid">
-      <HookContainer title="Change document title">
-        <TitleDemo />
-      </HookContainer>
+const App = () => {
+  useEffect(() => {
+    hotjar.initialize({ id: 5194295, sv: 0 });
+    console.log("init hotjar");
+  }, []);
 
-      <HookContainer title="Default value">
-        <DefaultValueDemo />
-      </HookContainer>
+  return (
+    <div>
+      <h1>useHooks rebuild</h1>
+      <p>
+        Project to build from scratch hooks from ui.dev{" "}
+        <a href="https://usehooks.com/" target="_blank">
+          useHooks
+        </a>
+      </p>
+      <div className="hooks-grid">
+        <HookContainer title="Change document title">
+          <TitleDemo />
+        </HookContainer>
 
-      <HookContainer title="Use toggle">
-        <ToggleDemo />
-      </HookContainer>
+        <HookContainer title="Default value">
+          <DefaultValueDemo />
+        </HookContainer>
 
-      <HookContainer title="Use previous">
-        <PreviousDemo />
-      </HookContainer>
+        <HookContainer title="Use toggle">
+          <ToggleDemo />
+        </HookContainer>
 
-      <HookContainer title="Browser preferred language">
-        <LanguageDemo />
-      </HookContainer>
+        <HookContainer title="Use previous">
+          <PreviousDemo />
+        </HookContainer>
 
-      <HookContainer title="Browser Favicon">
-        <FaviconDemo />
-      </HookContainer>
+        <HookContainer title="Browser preferred language">
+          <LanguageDemo />
+        </HookContainer>
 
-      <HookContainer title="Copy to clipboard">
-        <CopyDemo />
-      </HookContainer>
+        <HookContainer title="Browser Favicon">
+          <FaviconDemo />
+        </HookContainer>
 
-      <HookContainer title="Use interval">
-        <IntervalDemo />
-      </HookContainer>
+        <HookContainer title="Copy to clipboard">
+          <CopyDemo />
+        </HookContainer>
 
-      <HookContainer title="Use counter">
-        <CounterDemo />
-      </HookContainer>
+        <HookContainer title="Use interval">
+          <IntervalDemo />
+        </HookContainer>
 
-      <HookContainer title="Use lock scroll">
-        <ScrollDemo />
-      </HookContainer>
+        <HookContainer title="Use counter">
+          <CounterDemo />
+        </HookContainer>
 
-      <HookContainer title="Use queue">
-        <QueueContainer />
-      </HookContainer>
+        <HookContainer title="Use lock scroll">
+          <ScrollDemo />
+        </HookContainer>
 
-      <HookContainer title="Use timeout">
-        <TimeoutDemo />
-      </HookContainer>
+        <HookContainer title="Use queue">
+          <QueueContainer />
+        </HookContainer>
 
-      <HookContainer title="Use window size">
-        <WindowSizeDemo />
-      </HookContainer>
+        <HookContainer title="Use timeout">
+          <TimeoutDemo />
+        </HookContainer>
 
-      <HookContainer title="Use document visibility change">
-        <VisibilityDemo />
-      </HookContainer>
+        <HookContainer title="Use window size">
+          <WindowSizeDemo />
+        </HookContainer>
 
-      <HookContainer title="Use list">
-        <ListDemo />
-      </HookContainer>
+        <HookContainer title="Use document visibility change">
+          <VisibilityDemo />
+        </HookContainer>
 
-      <HookContainer title="Use object state">
-        <ObjectStateDemo />
-      </HookContainer>
+        <HookContainer title="Use list">
+          <ListDemo />
+        </HookContainer>
 
-      <HookContainer title="Use debounce">
-        <DebounceDemo />
-      </HookContainer>
+        <HookContainer title="Use object state">
+          <ObjectStateDemo />
+        </HookContainer>
 
-      <HookContainer title="Use continuous retry">
-        <RetryDemo />
-      </HookContainer>
+        <HookContainer title="Use debounce">
+          <DebounceDemo />
+        </HookContainer>
 
-      <HookContainer title="Use history state">
-        <HistoryStateDemo />
-      </HookContainer>
+        <HookContainer title="Use continuous retry">
+          <RetryDemo />
+        </HookContainer>
 
-      <HookContainer title="Use random interval">
-        <RandomIntervalDemo />
-      </HookContainer>
+        <HookContainer title="Use history state">
+          <HistoryStateDemo />
+        </HookContainer>
 
-      <HookContainer title="Event listener">
-        <EventListenerDemo />
-      </HookContainer>
+        <HookContainer title="Use random interval">
+          <RandomIntervalDemo />
+        </HookContainer>
 
-      <HookContainer title="Media query">
-        <MediaQueryDemo />
-      </HookContainer>
+        <HookContainer title="Event listener">
+          <EventListenerDemo />
+        </HookContainer>
 
-      <HookContainer title="Conditional interval">
-        <WhenIntervalDemo />
-      </HookContainer>
+        <HookContainer title="Media query">
+          <MediaQueryDemo />
+        </HookContainer>
 
-      <HookContainer title="Mouse position">
-        <MouseDemo />
-      </HookContainer>
+        <HookContainer title="Conditional interval">
+          <WhenIntervalDemo />
+        </HookContainer>
 
-      <HookContainer title="Click away">
-        <ClickAwayDemo />
-      </HookContainer>
+        <HookContainer title="Mouse position">
+          <MouseDemo />
+        </HookContainer>
 
-      <HookContainer title="Window scroll">
-        <WindowScrollDemo />
-      </HookContainer>
+        <HookContainer title="Click away">
+          <ClickAwayDemo />
+        </HookContainer>
 
-      <HookContainer title="Use logger">
-        <UseLoggerDemo />
-      </HookContainer>
+        <HookContainer title="Window scroll">
+          <WindowScrollDemo />
+        </HookContainer>
 
-      <HookContainer title="Screen orientation">
-        <ScreenOrientationDemo />
-      </HookContainer>
+        <HookContainer title="Use logger">
+          <UseLoggerDemo />
+        </HookContainer>
 
-      <HookContainer title="Use battery">
-        <BatteryDemo />
-      </HookContainer>
+        <HookContainer title="Screen orientation">
+          <ScreenOrientationDemo />
+        </HookContainer>
 
-      <HookContainer title="Use page leave">
-        <PageLeaveDemo />
-      </HookContainer>
+        <HookContainer title="Use battery">
+          <BatteryDemo />
+        </HookContainer>
+
+        <HookContainer title="Use page leave">
+          <PageLeaveDemo />
+        </HookContainer>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,24 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { hotjar } from "react-hotjar";
-
-hotjar.initialize({ id: 5194295, sv: 2 });
-
-// Identify the user
-hotjar.identify("USER_ID", { userProperty: "value" });
-
-// Add an event
-hotjar.event("button-click");
-
-// Update SPA state
-hotjar.stateChange("/my/page");
-
-// Check if Hotjar has been initialized before calling its methods
-if (hotjar.initialized()) {
-  console.log("initialized");
-  hotjar.identify("USER_ID", { userProperty: "value" });
-}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
